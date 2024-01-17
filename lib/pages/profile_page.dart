d00e06e4b46ab782f4c3b679ca0c2e41ec899bff
abellilo <abel.ayinde@gmail.com>
2024-01-17 17:13:03 +0100

    profile post descending now

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -63,11 +63,9 @@ class _ProfilePageState extends State<ProfilePage> {
         });
 
     //update in firestore
-    if(newValue.trim().isNotEmpty){
+    if (newValue.trim().isNotEmpty) {
       //only update is there is something in the textfield
-      await userCollections.doc(currentUser.email).update({
-        field : newValue
-      });
+      await userCollections.doc(currentUser.email).update({field: newValue});
     }
   }
 
@@ -75,153 +73,154 @@ class _ProfilePageState extends State<ProfilePage> {
   Widget build(BuildContext context) {
     return Scaffold(
         backgroundColor: Colors.brown[300],
-      appBar: AppBar(
-        title: Text(
-          "Profile Page",
-          style: TextStyle(color: Colors.white),
+        appBar: AppBar(
+          title: Text(
+            "Profile Page",
+            style: TextStyle(color: Colors.white),
+          ),
+          backgroundColor: Colors.grey[900],
+          leading: IconButton(
+            icon: Icon(
+              Icons.arrow_back,
+              color: Colors.white,
+            ),
+            onPressed: () => Navigator.pop(context),
+          ),
         ),
-        backgroundColor: Colors.grey[900],
-        leading: IconButton(
-          icon: Icon(
-            Icons.arrow_back,
-            color: Colors.white,
-          ),
-          onPressed: () => Navigator.pop(context),
-        ),
-      ),
-      body: ListView(
-        children: [
-          const SizedBox(
-            height: 50,
-          ),
-
-          //profile pic
-          Center(
-            child: Icon(
-              Icons.person,
-              size: 72,
+        body: ListView(
+          children: [
+            const SizedBox(
+              height: 50,
             ),
-          ),
 
-          const SizedBox(
-            height: 10,
-          ),
-
-          //user email
-          Center(
-            child: Text(
-              currentUser.email!,
-              textAlign: TextAlign.center,
-              style: TextStyle(color: Colors.white),
+            //profile pic
+            Center(
+              child: Icon(
+                Icons.person,
+                size: 72,
+              ),
             ),
-          ),
 
-          const SizedBox(
-            height: 50,
-          ),
-
-          //user details
-          Padding(
-            padding: EdgeInsets.only(left: 25),
-            child: Text(
-              "My Details",
-              style: TextStyle(color: Colors.white),
+            const SizedBox(
+              height: 10,
             ),
-          ),
 
-          StreamBuilder<DocumentSnapshot>(
-            stream: FirebaseFirestore.instance
-                .collection("Users")
-                .doc(currentUser.email)
-                .snapshots(),
-            builder: (context, snapshot) {
-              //get data
-              if (snapshot.hasData) {
-                Map<String, dynamic> userData =
-                snapshot.data!.data() as Map<String, dynamic>;
-                return Column(
-                  children: [
-
-                    //username
-                    MyTextBox(
-                      text: userData['username'],
-                      sectionName: "username",
-                      onTap: () => editField("username"),
-                    ),
-
-                    //bio
-                    MyTextBox(
-                      text: userData['bio'],
-                      sectionName: "bio",
-                      onTap: () => editField("bio"),
-                    ),
-                  ],
-                );
-              }
-              if (snapshot.hasError) {
-                return Center(
-                  child: Text("Error " + snapshot.hasError.toString()),
-                );
-              }
-              return Center(
-                child: Text("loading....",style: TextStyle(
-                    color: Colors.yellow
-                ),),
-              );
-            },
-          ),
-
-          const SizedBox(
-            height: 50,
-          ),
-
-          //user posts
-          Padding(
-            padding: EdgeInsets.only(left: 25),
-            child: Text(
-              "My Posts",
-              style: TextStyle(color: Colors.white),
+            //user email
+            Center(
+              child: Text(
+                currentUser.email!,
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.white),
+              ),
             ),
-          ),
 
-          //show my post
-          StreamBuilder(
-              stream: FirebaseFirestore.instance.collection("User Posts").snapshots(),
-              builder: (context, snapshot){
-                if(snapshot.hasData){
-                  final posts = snapshot.data!.docs;
-                  return ListView.builder(
-                      shrinkWrap: true,
-                      physics: NeverScrollableScrollPhysics(),
-                      itemCount: posts.length,
-                      itemBuilder: (context, index){
-                        final post = posts[index];
-                        if(currentUser.email == post['UserEmail']){
-                          return Post(
-                              time: formatData(post['Timestamp']),
-                              message: post['Message'],
-                              user: post['UserEmail'],
-                              likes: post['Likes'],
-                              postId: post.id
-                          );
-                        }
-                        else{
-                          return Container();
-                        }
-                      });
-                }
-                if(snapshot.hasError){
-                  return Center(
-                    child: Text("Error "+snapshot.hasError.toString()),
+            const SizedBox(
+              height: 50,
+            ),
+
+            //user details
+            Padding(
+              padding: EdgeInsets.only(left: 25),
+              child: Text(
+                "My Details",
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+
+            StreamBuilder<DocumentSnapshot>(
+              stream: FirebaseFirestore.instance
+                  .collection("Users")
+                  .doc(currentUser.email)
+                  .snapshots(),
+              builder: (context, snapshot) {
+                //get data
+                if (snapshot.hasData) {
+                  Map<String, dynamic> userData =
+                      snapshot.data!.data() as Map<String, dynamic>;
+                  return Column(
+                    children: [
+                      //username
+                      MyTextBox(
+                        text: userData['username'],
+                        sectionName: "username",
+                        onTap: () => editField("username"),
+                      ),
+
+                      //bio
+                      MyTextBox(
+                        text: userData['bio'],
+                        sectionName: "bio",
+                        onTap: () => editField("bio"),
+                      ),
+                    ],
                   );
                 }
-                return Center(child: Text("loading...",style: TextStyle(
-                  color: Colors.yellow
-                ),));
-              }
-          ),
-        ],
-      )
-    );
+                if (snapshot.hasError) {
+                  return Center(
+                    child: Text("Error " + snapshot.hasError.toString()),
+                  );
+                }
+                return Center(
+                  child: Text(
+                    "loading....",
+                    style: TextStyle(color: Colors.yellow),
+                  ),
+                );
+              },
+            ),
+
+            const SizedBox(
+              height: 50,
+            ),
+
+            //user posts
+            Padding(
+              padding: EdgeInsets.only(left: 25),
+              child: Text(
+                "My Posts",
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+
+            //show my post
+            StreamBuilder(
+                stream: FirebaseFirestore.instance
+                    .collection("User Posts")
+                    .orderBy('Timestamp', descending: true)
+                    .snapshots(),
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    final posts = snapshot.data!.docs;
+                    return ListView.builder(
+                        shrinkWrap: true,
+                        physics: NeverScrollableScrollPhysics(),
+                        itemCount: posts.length,
+                        itemBuilder: (context, index) {
+                          final post = posts[index];
+                          if (currentUser.email == post['UserEmail']) {
+                            return Post(
+                                time: formatData(post['Timestamp']),
+                                message: post['Message'],
+                                user: post['UserEmail'],
+                                likes: post['Likes'],
+                                postId: post.id);
+                          } else {
+                            return Container();
+                          }
+                        });
+                  }
+                  if (snapshot.hasError) {
+                    return Center(
+                      child: Text("Error " + snapshot.hasError.toString()),
+                    );
+                  }
+                  return Center(
+                      child: Text(
+                    "loading...",
+                    style: TextStyle(color: Colors.yellow),
+                  ));
+                }),
+          ],
+        ));
   }
 }
